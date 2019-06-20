a03b2f4e4a770797d0cf48a2da762e3c1a4e010a
Gallen Calade <gallencalade@qq.com>
2019-06-20 23:35:40 +0800

    init logger

---
+++ src/logger/logger.h
@@ -0,0 +1,14 @@
+#ifndef WEAPONRY_SRC_LOGGER_H
+#define WEAPONRY_SRC_LOGGER_H
+
+namespace weaponry {
+
+class Logger {
+ public:
+
+ private:
+};
+
+}   // namespace
+
+#endif    // WEAPONRY_SRC_LOGGER_H
